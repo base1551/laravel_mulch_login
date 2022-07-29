1dc38ee96bbf97445f5ebf02f2b91e87de983ba5
base1551 <baseball.1.kyoto@gmail.com>
2022-07-29 22:27:22 +0900

    gameテーブル作成

--- database/migrations/2022_07_29_215627_create_teams_table.php
+++ database/migrations/2022_07_29_215627_create_teams_table.php
@@ -15,7 +15,7 @@ class CreateTeamsTable extends Migration
     {
         Schema::create('teams', function (Blueprint $table) {
             $table->id();
-            $table->text('name');
+            $table->string('name');
             $table->timestamps();
         });
     }
---
+++ database/migrations/2022_07_29_221444_create_games_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('first_team_id')->constrained('teams');
+            $table->foreignId('second_team_id')->constrained('teams');
+
+            $table->integer('first_team_score')->nullable();
+            $table->integer('second_team_score')->nullable();
+            $table->integer('first_team_hits')->nullable();
+            $table->integer('socond_team_hits')->nullable();
+            $table->integer('first_team_errors')->nullable();
+            $table->integer('second_team_errors')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+};
